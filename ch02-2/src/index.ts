098becb27938e2a849de15474b3be7dd4b697141
KimDonghyeon <minseok0130@gmail.com>
2024-04-09 01:07:57 +0900

    chance ramda 사용

--- ch02-2/src/index.ts
+++ ch02-2/src/index.ts
@@ -1,10 +1,13 @@
 import IPerson from './person/IPerson'
 import Person, { makePerson } from './person/Person'
+import Chance from 'chance'
+import * as R from 'ramda'
 
-const testMakePerson = () => {
-  let jane: IPerson = makePerson('Jane')
-  let jack: IPerson = makePerson('Jack')
-  console.log(jane, jack)
-}
+/*
+chance: fake data 만드는데 사용
+ramda: 함수형 유틸리티 패키지
+ */
 
-testMakePerson()
+const chance = new Chance()
+let persons: IPerson[] = R.range(0, 2).map((n: number) => new Person(chance.name(), chance.age()))
+console.log(persons)
