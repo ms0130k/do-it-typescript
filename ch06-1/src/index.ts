cecb7d47dd7239c4287a366a77e95a44eab81ee8
KimDonghyeon <minseok0130@gmail.com>
2024-04-11 08:38:37 +0900

    RangeIterable 클래스 작성 준비

--- ch06-1/src/index.ts
+++ ch06-1/src/index.ts
@@ -1,17 +1,1 @@
 console.log('Hello TypeScript');
-
-const func = () => ({a: 1, b: this})
-console.log(func())
-const func2 = function () {
-  return {a: 1, b: this}
-}
-console.log(func2())
-const func3 = function () {
-  return {
-    a: 1,
-    b() {
-      console.log(this)
-    }
-  }
-}
-func3().b()
