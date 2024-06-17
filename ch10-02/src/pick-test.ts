aedb0b5fd0ebe90e3420dd9088570766f74a9265
KimDonghyeon <minseok0130@gmail.com>
2024-06-17 20:42:34 +0900

    type constraint

---
+++ ch10-02/src/pick-test.ts
@@ -0,0 +1,9 @@
+import {pick} from './pick';
+
+const obj = {name: 'Jane', age: 22, city:'Seoul', country: 'Korea'}
+console.log(
+  pick(obj, ['name', 'age']),
+  // pick(obj, ['nam', 'aggge'])
+);
+
+// <T, K extends keyof T>
--- ch10-02/src/pick.ts
+++ ch10-02/src/pick.ts
@@ -1,13 +1,6 @@
-export const pick = (obj, keys) => keys.map(key => ({[key]: obj[key]}))
-  .reduce((result, value) => ({...result, ...value}), {});
+export const pick = <T, K extends keyof T>(obj: T, keys: K[]) =>
+  keys.map(key => ({[key]: obj[key]}))
+    .reduce((result, value) => ({...result, ...value}), {});
 
-// const obj1 = {a: 1, b: 2, c: 3};
-// const obj2 = {a: 2, d: 4, e: 5, f: 6};
-//
-// const obj3 = {...obj1, ...obj2};
-//
-//
-// const pick = (obj: {}, keys: any[]) => keys.map(key => ({[key]: obj[key]}))
-//   .reduce((result, curr) => ({...result, ...curr}), {});
-//
-// console.log(pick(obj2, ['a', 'e']))
+const obj = {a: 'aaa', b: 'bbb', c: 'cccccc'}
+pick(obj, ['a', 'b']);
